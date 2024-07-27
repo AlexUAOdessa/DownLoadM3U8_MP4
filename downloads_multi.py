49ccb211befc9a1fe2ced4f0c5363a6121339e1a
AlexUAOdessa <admin1alex@gmail.com>
2024-07-27 16:28:20 +0300

    beta1

--- downloads_multi.py
+++ downloads_multi.py
@@ -4,43 +4,39 @@ import subprocess
 import time
 from concurrent.futures import ThreadPoolExecutor
 
-# Функция для скачивания файла
-
-
+# Функция для скачивания файла с повторами
 def download_file(url, file_path, timeout=60):
-    start_time = time.time()
-    downloaded_size = 0
-    try:
-        response = requests.get(url, stream=True, timeout=timeout)
-        response.raise_for_status()  # Проверка на ошибки HTTP
-        total_size = int(response.headers.get('content-length', 0))
+    while True:
+        start_time = time.time()
+        downloaded_size = 0
+        try:
+            response = requests.get(url, stream=True, timeout=timeout)
+            response.raise_for_status()  # Проверка на ошибки HTTP
+            total_size = int(response.headers.get('content-length', 0))
 
-        with open(file_path, 'wb') as file:
-            for data in response.iter_content(chunk_size=1024):
-                if time.time() - start_time > timeout:
-                    raise Exception("Загрузка прервана, слишком долго")
-                file.write(data)
-                downloaded_size += len(data)
-                done = int(50 * downloaded_size / total_size)
-                percent_done = (downloaded_size / total_size) * 100
-                elapsed_time = time.time() - start_time
-                speed = downloaded_size / elapsed_time if elapsed_time > 0 else 0
+            with open(file_path, 'wb') as file:
+                for data in response.iter_content(chunk_size=1024):
+                    if time.time() - start_time > timeout:
+                        raise Exception("Загрузка прервана, слишком долго")
+                    file.write(data)
+                    downloaded_size += len(data)
+                    done = int(50 * downloaded_size / total_size)
+                    percent_done = (downloaded_size / total_size) * 100
+                    elapsed_time = time.time() - start_time
+                    speed = downloaded_size / elapsed_time if elapsed_time > 0 else 0
 
-                # Форматирование строки
-                print(f"\rЗагрузка {file_path}: {downloaded_size / (1024 * 1024):.2f} MB из {total_size / (
-                    1024 * 1024):.2f} MB): [{'#' * done}{'.' * (50 - done)}] {percent_done:.2f}%", end='')
+                    # Форматирование строки
+                    print(f"\rЗагрузка {file_path}: {downloaded_size / (1024 * 1024):.2f} MB из {total_size / (1024 * 1024):.2f} MB): [{'#' * done}{'.' * (50 - done)}] {percent_done:.2f}%", end='')
 
-            print()  # Завершение строки после завершения загрузки
-        return True  # Успешная загрузка
-    except (requests.RequestException, Exception) as e:
-        print(f"\nОшибка при скачивании {file_path}: {e}")
-        if os.path.exists(file_path):
-            os.remove(file_path)
-        return False
+                print()  # Завершение строки после завершения загрузки
+            return True  # Успешная загрузка
+        except (requests.RequestException, Exception) as e:
+            print(f"\nОшибка при скачивании {file_path}: {e}")
+            if os.path.exists(file_path):
+                os.remove(file_path)
+            time.sleep(5)  # Подождем перед повторной попыткой
 
 # Функция для конвертации файлов сегментов в mp4
-
-
 def convert_segments_to_mp4(segment_files, mp4_file):
     try:
         # Создание файла filelist.txt
@@ -54,8 +50,7 @@ def convert_segments_to_mp4(segment_files, mp4_file):
             print(filelist.read())
 
         # Выполнение команды ffmpeg
-        command = ['ffmpeg', '-y', '-f', 'concat', '-safe',
-                   '0', '-i', 'filelist.txt', '-c', 'copy', mp4_file]
+        command = ['ffmpeg', '-y', '-f', 'concat', '-safe', '0', '-i', 'filelist.txt', '-c', 'copy', mp4_file]
         result = subprocess.run(command, text=True, capture_output=True)
         if result.returncode != 0:
             print(f"Ошибка при конвертации: {result.stderr}")
@@ -64,14 +59,11 @@ def convert_segments_to_mp4(segment_files, mp4_file):
     except subprocess.CalledProcessError as e:
         print(f"Ошибка при конвертации сегментов в {mp4_file}: {e}")
     except Exception as e:
-        print(f"Неизвестная ошибка при конвертации сегментов в {
-              mp4_file}: {e}")
+        print(f"Неизвестная ошибка при конвертации сегментов в {mp4_file}: {e}")
     finally:
         os.remove('filelist.txt')  # Удаление временного файла
 
 # Функция для скачивания m3u8 файла
-
-
 def download_m3u8(m3u8_url, output_path):
     try:
         response = requests.get(m3u8_url)
@@ -82,8 +74,6 @@ def download_m3u8(m3u8_url, output_path):
         print(f"Ошибка при скачивании m3u8 файла: {e}")
 
 # Функция для парсинга m3u8 файла
-
-
 def parse_m3u8(m3u8_file):
     segments = []
     with open(m3u8_file, 'r') as f:
@@ -96,8 +86,6 @@ def parse_m3u8(m3u8_file):
     return segments
 
 # Функция для многопоточного скачивания сегментов
-
-
 def download_segments(segments, base_url, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -107,12 +95,10 @@ def download_segments(segments, base_url, output_dir):
         for i, segment in enumerate(segments):
             segment_url = f"{base_url}/{segment}".replace(' ', '%20')
             segment_path = os.path.join(output_dir, segment)
-            futures.append(executor.submit(
-                download_file, segment_url, segment_path))
+            futures.append(executor.submit(download_file, segment_url, segment_path))
 
         for future in futures:
             future.result()  # Дожидаемся завершения всех потоков
-
 
 # Основной блок для обработки ссылок
 if __name__ == "__main__":
@@ -160,5 +146,4 @@ if __name__ == "__main__":
 
         # Вывод информации о завершенной загрузке
         remaining_files = len(lines) - index
-        print(f'Завершена загрузка: {mp4_file_path}. Осталось {
-              remaining_files} файлов.')
+        print(f'Завершена загрузка: {mp4_file_path}. Осталось {remaining_files} файлов.')
